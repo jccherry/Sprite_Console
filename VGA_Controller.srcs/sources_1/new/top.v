4d5b29f1ab86b1310ee1ead443b812775384a5ee
John Chiaramonte <chiaramonte.j@northeastern.edu>
2020-04-25 16:23:52 -0400

    added snes controller support, broke project files and put back together again

--- VGA_Controller.srcs/sources_1/new/top.v
+++ VGA_Controller.srcs/sources_1/new/top.v
@@ -23,12 +23,10 @@
 module top(
     input sys_clk,
     input [15:0] sw,
-    input btnU,
-    input btnD,
-    input btnL,
-    input btnR,
-    output [7:0] cathode_data_out,
-    output [3:0] enabled_segment,
+    input snes_data_in,
+    output snes_clk,
+    output reg snes_latch,
+    output [11:0] snes_data_out,
     output hsync,
     output vsync,
     output [3:0] R,
@@ -58,45 +56,17 @@ module top(
     
     always@(posedge vblank)
     begin
-        if(btnU && ~btnD)
-            sprite_y = sprite_y - 1;
-        else if (btnD)
-            sprite_y  = sprite_y + 1;
-        
-        if(btnR && ~btnL)
-        begin
-            if (sprite_x >= 515)
-            begin
-                foreground_offset = foreground_offset + 1;
-                //if fg_offset is even (lsb), add 1 to bg offset
-                if (foreground_offset[0] == 0)
-                    bg_offset = bg_offset + 1;
-            end
-            else
-                sprite_x = sprite_x + 1;
-        end
-        else if (btnL)
-        begin
-            if (sprite_x <= 128)
-            begin
-                foreground_offset = foreground_offset - 1;
-                //if fg_offset is even (lsb), add 1 to bg offset
-                if (foreground_offset[0] == 0 && foreground_offset > 0)
-                    bg_offset = bg_offset - 1;
-            end
-            else
-                sprite_x = sprite_x - 1;
-        end
-        
+        if(snes_data_out[0] == 1'b1)
+            sprite_y = sprite_y-1;
+        else if(snes_data_out[1] == 1'b1)
+            sprite_y = sprite_y+1;
+            
+        if(snes_data_out[2] == 1'b1)
+            sprite_x = sprite_x-1;
+        else if(snes_data_out[3] == 1'b1)
+            sprite_x = sprite_x+1;
     end
     
-    wire [15:0] seven_seg_input;
-    //assign seven_seg_input[15:8] = bg_offset;
-    assign seven_seg_input[10:0] = foreground_offset;
-    assign seven_seg_input[11] = 0;
-    assign seven_seg_input[15:12] = sw[2:0];
-    
-    wire vga_clk;
 
     ppu ppu(
     clk_25MHz,
@@ -115,13 +85,111 @@ module top(
     G,
     B
     );
-    /*
-    seven_segment_scanner seg(
-    vblank,
-    seven_seg_input,
-    4'b0000,
-    cathode_data_out,
-    enabled_segment
-    );*/
     
+        //SNES CONTROLLER HANDLER=========================================================
+    wire slow_clk;
+    reg [4:0] counter;
+    wire snes_clk_started;
+    reg [11:0] snes_data;
+    reg enable_snes_clk_output;
+    
+    
+    snes_clock_divider clkdiv(
+    sys_clk,
+    slow_clk
+    );
+    
+    //assign slow_clk = sys_clk;
+    
+    initial begin
+        enable_snes_clk_output = 0;
+        snes_latch = 0;
+        snes_data = 0;
+        counter = 0;
+    end
+
+    //when read is enabled, this clock starts, otherwise its high    
+    assign snes_clk_started = (sw[15]) ? slow_clk : 1'b0;
+    
+    //when output is enabled, controller will start getting clock
+    //(12us after latch is high, clock signal is enabled)
+    assign snes_clk = (enable_snes_clk_output) ? snes_clk_started : 1'b1;
+
+    always@(posedge snes_clk_started)
+    begin
+        case (counter)
+            0   :   begin
+                        snes_latch = 1'b1;
+                        counter = counter + 1;
+                    end
+            1   :   begin
+                        snes_latch = 1'b0;
+                        enable_snes_clk_output = 1;
+                        counter = counter + 1;
+                    end
+            17  :   begin
+                        enable_snes_clk_output = 0;
+                        counter = 0;
+                    end
+            default : counter = counter + 1;    
+        endcase 
+    end
+    
+    always @(negedge snes_clk)
+    begin
+        case (counter)
+            
+            2 : snes_data[0] = ~snes_data_in; //b button
+            3 : snes_data[1] = ~snes_data_in; //y buton
+            4 : snes_data[2] = ~snes_data_in; //sel
+            5 : snes_data[3] = ~snes_data_in; //  start
+            6 : snes_data[4] = ~snes_data_in; // up
+            7 : snes_data[5] = ~snes_data_in; // down
+            8 : snes_data[6] = ~snes_data_in; // left
+            9 : snes_data[7] = ~snes_data_in; //rigjht
+            10: snes_data[8] = ~snes_data_in; //a
+            11: snes_data[9] = ~snes_data_in; //x
+            12: snes_data[10] = ~snes_data_in; //L
+            13: snes_data[11] = ~snes_data_in; //R
+        endcase
+    end
+    
+    
+    //mapping reg to wires for ease of use later
+    assign snes_data_out[0] = snes_data[4]; //up
+    assign snes_data_out[1] = snes_data[5]; //down
+    assign snes_data_out[2] = snes_data[6]; //left
+    assign snes_data_out[3] = snes_data[7]; //right
+    assign snes_data_out[4] = snes_data[8]; //a
+    assign snes_data_out[5] = snes_data[0]; //b
+    assign snes_data_out[6] = snes_data[9]; //x
+    assign snes_data_out[7] = snes_data[1]; //y
+    assign snes_data_out[8] = snes_data[3]; //start
+    assign snes_data_out[9] = snes_data[2]; //sel
+    assign snes_data_out[10] = snes_data[10]; //L
+    assign snes_data_out[11] = snes_data[11]; //R
+    
+    //SNES CONTROLLER HANDLER=========================================================
+
+endmodule
+
+module snes_clock_divider(clock_in,clock_out
+    );
+input clock_in; // input clock on FPGA
+output clock_out; // output clock after dividing the input clock by divisor
+reg[27:0] counter=28'd0;
+parameter DIVISOR = 28'd400;
+//parameter DIVISOR = 28'd1;
+// The frequency of the output clk_out
+//  = The frequency of the input clk_in divided by DIVISOR
+// For example: Fclk_in = 50Mhz, if you want to get 1Hz signal to blink LEDs
+// You will modify the DIVISOR parameter value to 28'd50.000.000
+// Then the frequency of the output clk_out = 50Mhz/50.000.000 = 1Hz
+always @(posedge clock_in)
+begin
+ counter <= counter + 28'd1;
+ if(counter>=(DIVISOR-1))
+  counter <= 28'd0;
+end
+assign clock_out = (counter<DIVISOR/2)?1'b0:1'b1;
 endmodule
